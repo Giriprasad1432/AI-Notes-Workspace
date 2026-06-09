891011b0cee37b9ceeb28790d3729593baac9f7f
Giri prasad <giribabu121989@gmail.com>
2026-06-10 03:14:18 +0530

    fix: ready for vercel deploy

--- frontend/ai-note-saver/vite.config.js
+++ frontend/ai-note-saver/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
-import path from 'path'
-// https://vite.dev/config/
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  root: __dirname,
   plugins: [react(), tailwindcss()],
 })
