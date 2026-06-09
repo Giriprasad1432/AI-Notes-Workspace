d7224428af64ff92f8ac273f55a5b9cbfdcbb3dd
Giri prasad <giribabu121989@gmail.com>
2026-06-10 02:18:24 +0530

    build issue resolved

--- frontend/ai note saver/vite.config.js
+++ frontend/ai note saver/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import path from 'path'
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  root: 'src',
-  plugins: [react(),tailwindcss()],
+  root: __dirname,
+  plugins: [react(), tailwindcss()],
 })
